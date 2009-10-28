5e359aedcfdb46bda6a782cb86638470be6aa055
matthias.ringwald <matthias.ringwald@1a0a8af8-31b5-11de-8e0c-53a27eea117e>
2009-10-28 22:05:46 +0000

    finish update script, check in BTstack r401

---
+++ debs/update.sh
@@ -0,0 +1,12 @@
+#/bin/sh
+dpkg-scanpackages . overridefile > Packages
+rm -f Packages.bz2
+bzip2 -k Packages
+P1_MD5=`md5sum Packages | cut -d " " -f 1`
+P1_SIZE=`ls -l Packages | cut -d " " -f 8`
+P2_MD5=`md5sum Packages.bz2 | cut -d " " -f 1`
+P2_SIZE=`ls -l Packages.bz2 | cut -d " " -f 8`
+rm Release
+cp Release.in Release
+echo " $P1_MD5 $P1_SIZE Packages" >> Release
+echo " $P2_MD5 $P2_SIZE Packages" >> Release
